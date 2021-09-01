18fe2ea5951defc5acf01314d2d88028fcd0ee29
Suraj Kumar Gupta <surajkumarg@maveric-systems.com>
2021-09-01 17:18:17 +0530

    Initial commits

---
+++ TFSCalenderFramework/TFSCalenderFramework/TFSCalenderFramework.h
@@ -0,0 +1,18 @@
+//
+//  TFSCalenderFramework.h
+//  TFSCalenderFramework
+//
+//  Created by Suraj Kumar Gupta on 01/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TFSCalenderFramework.
+FOUNDATION_EXPORT double TFSCalenderFrameworkVersionNumber;
+
+//! Project version string for TFSCalenderFramework.
+FOUNDATION_EXPORT const unsigned char TFSCalenderFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TFSCalenderFramework/PublicHeader.h>
+
+
